6c360a4a08a4176996fcf4454dd0df6b8705b57c
karoljoniak <karol.joniak1@gmail.com>
2023-02-24 19:03:17 +0100

    test

---
+++ src/components/Input.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Input.module.scss';
+
+const Input = () => {
+    return (
+        <div className={styles.test}>
+
+        </div>
+    );
+};
+
+export default Input;
